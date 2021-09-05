aa06005fe6c4a459f4fc88a136005ab316b20674
Carlos <carlos.gomesystems@gmail.com>
2021-09-05 00:02:06 -0300

    Modelagem casa de repouso

---
+++ CasaRepouso/Models/CondiçãoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaRepouso.Models
+{
+    public class CondiçãoModel
+    {
+        public int CondicaoId { get; set; }
+        public string CondicaoDesc { get; set; }
+    }
+}
---
+++ CasaRepouso/Models/PacienteModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CasaRepouso.Models
+{
+    public class PacienteModel
+    {
+        public int PacienteId { get; set; }
+        public string PacienteNome { get; set; }
+        public string PacienteDesc { get; set; }
+        public string PacienteDtInclusao { get; set; }
+    }
+}
